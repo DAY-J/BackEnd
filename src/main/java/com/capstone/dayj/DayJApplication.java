6e95f62b180f03655c5bf4f885d4ecb125b4b8ab
DNA-B <sphz3813@gmail.com>
2024-09-18 02:03:23 +0900

    Refactor: Spring default Timezone fix (Asia/Seoul)

--- src/main/java/com/capstone/dayj/DayJApplication.java
+++ src/main/java/com/capstone/dayj/DayJApplication.java
@@ -1,16 +1,24 @@
 package com.capstone.dayj;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @EnableJpaAuditing
 @EnableScheduling // 스케줄러 활성화
 @ConfigurationPropertiesScan
 @SpringBootApplication
 public class DayJApplication {
+    @PostConstruct
+    public void setTimeZone() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+    
     public static void main(String[] args) {
         SpringApplication.run(DayJApplication.class, args);
     }
